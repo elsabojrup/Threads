df5772968a4529635aa5b0dafff9acd68fb1f09c
elsabojrup <bojrup.elsa@gmail.com>
2024-09-07 17:25:20 +0200

    Send PM functionality

--- src/com/elsab/simpleserver/ClientHandler.java
+++ src/com/elsab/simpleserver/ClientHandler.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class ClientHandler extends Thread {
     private Socket socket;
+    private String username;
+    private PrintWriter out;
+    private BufferedReader in;
 
-    public ClientHandler(Socket socket) {
+    public ClientHandler(Socket socket, String username) {
         this.socket = socket;
+        this.username = username;
+
     }
 
     @Override
@@ -19,15 +25,65 @@ public class ClientHandler extends Thread {
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
+            this.in = in;
+            this.out = out;
+
             String message;
             while ((message = in.readLine()) != null) {
-                System.out.println("Recieved: " + message);
+                if(message.startsWith("@"))
+                {
+                    int spaceIndex = message.indexOf(" ");
+                    String recipient = message.substring(1, spaceIndex);
+                    String pm = message.substring(spaceIndex+1);
+                    sendPrivateMessage(recipient, pm);
+                }
+                System.out.println("Recieved from " + username +": " + message);
                 out.println("Echo: " + message);
             }
 
+        }catch (SocketException e) {
+            System.out.println("Connection reset by client " + username + ": " + e.getMessage());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            MultiThreadedServer.removeClientHandler(username);
+            try{
+                socket.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        }
+
+    private void sendPrivateMessage(String recipient, String pm) {
+        ClientHandler recieptHandler = MultiThreadedServer.getClientHandler(recipient);
+        if(recieptHandler != null){
+            recieptHandler.sendMessage("@" + username + " " + pm);
+        }
+        else{
+            sendMessage(recipient +" not found. Sorry...");
+        }
+    }
+
+    public void sendMessage(String message){
+        out.println(message);
+    }
+
+    public String senPM(String message){
+        out.println(message);
+        String input = "";
+        while(!input.matches("^(y|yes|n|no)$"))
+        try {
+            input = in.readLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return input;
+    }
 
+    public String getUsername() {
+        return username;
     }
 }
--- src/com/elsab/simpleserver/MultiThreadedServer.java
+++ src/com/elsab/simpleserver/MultiThreadedServer.java
@@ -1,11 +1,17 @@
 package com.elsab.simpleserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MultiThreadedServer {
     private int port;
+    private static Map<String, ClientHandler> clientHandlers = new HashMap<>();
 
     MultiThreadedServer(int port){
         this.port = port;
@@ -18,11 +24,55 @@ public class MultiThreadedServer {
             while(true){
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client added. Welcoming " + clientSocket.getRemoteSocketAddress());
-                new ClientHandler(clientSocket).start();
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                out.println("Enter username.");
+                String username = in.readLine();
+                if(username == null || username.trim().isEmpty()){
+                    System.out.println("User did not enter a name.");
+                    out.println("You did not enter a username. Bye!");
+                    clientSocket.close();
+                    continue;
+                }
+                ClientHandler client = new ClientHandler(clientSocket, username);
+                addClientHandler(username, client);
+                out.println("Welcome " +username+ "!");
+                client.start();
+                broadcastMessages(username + " has joined the chat.\n If you want to chat with " +username+ ", type @"+username, username);
+
             }
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void addClientHandler(String userName, ClientHandler clientHandler){
+        clientHandlers.put(userName, clientHandler);
+    }
+
+    public static ClientHandler getClientHandler(String username){
+        return clientHandlers.get(username);
+    }
+
+    public static void removeClientHandler(String username){
+        clientHandlers.remove(username);
+    }
+
+    public void connectUsers(String username){
+        for(ClientHandler clientHandler : clientHandlers.values() ){
+            if(clientHandler.getUsername() != username ){
+                String answer = clientHandler.senPM("Do you want to chat with " + username + "? (y/n)");
+            }
+        }
+    }
+
+    public void broadcastMessages(String message, String username){
+        for(ClientHandler clientHandler : clientHandlers.values()){
+            if(clientHandler.getUsername() != username){
+            clientHandler.sendMessage(message);
+            }
         }
     }
 
